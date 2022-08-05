673f7cf772d267f0bff9425f4266fe48bd0b27a3
Oleg <vetti.ch@ya.ru>
2022-08-06 04:24:29 +0500

    feat: add accounts

--- src/store/accounts.ts
+++ src/store/accounts.ts
@@ -1,4 +1,5 @@
 import { writable } from 'svelte/store';
+import { get, post } from '../api';
 
 interface AccountsWrapper {
 	loading: boolean,
@@ -7,11 +8,35 @@ interface AccountsWrapper {
 
 interface Account {
 	id: string,
+	name: string,
+	type: string,
+	photo?: string,
 }
 
 function createAccounts() {
 	const initial: AccountsWrapper = { loading: true }
 	const { subscribe, set, update } = writable(initial);
+
+	const setLoading = (loading: boolean) => {
+		update(store => ({...store, loading}))
+	}
+
+	const load = async () => {
+		setLoading(true);
+		get('accounts')
+			.then(res => {
+				update(store => ({
+					...store,
+					data: res.accounts,
+				}))
+			})
+			.catch(e => console.log(e))
+			.finally(() => {
+				setLoading(false);
+			})
+	}
+
+	load();
 
 	return {
 		subscribe,
