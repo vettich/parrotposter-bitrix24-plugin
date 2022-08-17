e3d453ffff0775ac1a03fdcd6f02d78cd6a3e9a0
Oleg <vetti.ch@ya.ru>
2022-08-17 12:17:24 +0500

    feat: group post by date

--- src/components/posts/PostList.svelte
+++ src/components/posts/PostList.svelte
@@ -1,20 +1,46 @@
 <script lang="ts">
-	import PostListItem from './PostListItem.svelte';
-	import type { Post } from '../store/post.ts';
+	import Item from './PostListItem.svelte';
+	import type { Post } from '../../types';
 
 	export let posts: Post[];
+
+	interface Group {
+		[key: string]: Post[],
+	}
+
+	const dateToStr = (d: Date) => `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
+
+	const formatDay = (d: Date | string) => {
+		d = new Date(d);
+		return d.toLocaleDateString();
+	}
+
+	$: group = posts.reduce((group: Group, post) => {
+		const { publish_at } = post;
+		const key = dateToStr(publish_at);
+		group[key] = group[key] ?? [];
+		group[key].push(post);
+		return group;
+	}, {})
 </script>
 
-<div class="posts">
-	{#each posts as post, idx (post.id)}
-		<PostListItem {post} />
-	{/each}
-</div>
+{#each Object.entries(group) as [date, posts] (date)}
+	<h2>{formatDay(date)}</h2>
+	<div class="posts">
+		{#each posts as post (post.id)}
+			<Item {post} />
+		{/each}
+	</div>
+{/each}
 
 <style lang="scss">
+	@use '../../theme/helpers' as *; 
+
 	.posts {
 		display: flex;
 		flex-wrap: wrap;
 		gap: 1em;
+		padding-left: 1em;
+		border-left: 2px solid cssvar(primary);
 	}
 </style>
