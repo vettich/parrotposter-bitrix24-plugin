045730783b9650565e7372ff607cb2d5c9f23d12
Oleg <vetti.ch@ya.ru>
2022-08-26 00:57:13 +0500

    feat: add post status tooltip text

--- src/components/posts/PostStatus.svelte
+++ src/components/posts/PostStatus.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
-	import { Icon } from '@smui/common';
 	import type { PostStatus } from '@src/types';
+
+	import { Icon } from '@smui/common';
 
 	export let status: PostStatus;
 
-	const statusIcons: { [key in PostStatus]: string } = {
+	const icons: { [key in PostStatus]: string } = {
 		success: 'done',
 		fail: 'error',
 		queue: 'schedule_send',
 		ready: 'pending',
-	}
+	};
+
+	const texts: { [key in PostStatus]: string } = {
+		success: 'Успешно',
+		fail: 'Ошибка',
+		queue: 'Публикуется',
+		ready: 'Ожидает подходящего времени',
+	};
 </script>
 
-<div
+<div title={texts[status]}
 	class="post-status post-status--{status}">
-	<Icon class="material-icons-outlined">{statusIcons[status]}</Icon>
+	<Icon class="material-icons-outlined">{icons[status]}</Icon>
 </div>
 
 <style lang="scss">
