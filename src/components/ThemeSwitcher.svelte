ebefaad164a4ae9acee406a8c8d4bdffccec970f
Oleg <vetti.ch@ya.ru>
2022-08-31 16:20:59 +0500

    fix: success shadow on acc icon when queue

--- src/components/ThemeSwitcher.svelte
+++ src/components/ThemeSwitcher.svelte
@@ -6,13 +6,14 @@
 	let isDark = true;
 
 	$: isDark = $theme === 'dark';
+	$: title = isDark ? 'Переключить на светлую тему' : 'Переключить на темную тему';
 
 	const switchTheme = () => {
 		$theme = isDark ? 'light' : 'dark';
 	}
 </script>
 
-<IconButton on:click={switchTheme} pressed={isDark}>
+<IconButton on:click={switchTheme} pressed={isDark} {title}>
 	<Icon class="material-icons-outlined" on>dark_mode</Icon>
 	<Icon class="material-icons-outlined">light_mode</Icon>
 </IconButton>
--- src/components/posts/PostListItemAccounts.svelte
+++ src/components/posts/PostListItemAccounts.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { accounts } from '@src/store';
 	import { mergeAccountsResults } from '@src/tools';
-	import type { PostResults } from '@src/types';
+	import type { PostResult, PostResults } from '@src/types';
 
 	import { AccountPhoto } from '@src/components/accounts';
 
@@ -9,11 +9,16 @@
 	export let results: PostResults;
 
 	$: items = mergeAccountsResults($accounts.data, accountIds, results);
+
+	function isSuccess(result: PostResult): boolean {
+		if (!result) return null;
+		return result.success;
+	}
 </script>
 
 <div class="post-list-item-accounts">
 	{#each items as item (item.id)}
-		<AccountPhoto id={item.id} account={item.account} success={item.result?.success} />
+		<AccountPhoto id={item.id} account={item.account} success={isSuccess(item.result)} />
 	{/each}
 </div>
 
